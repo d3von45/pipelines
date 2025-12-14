289b9ee10ddd7ccf367ff327b4490f2900e48de4
EC2 Default User <ec2-user@ip-10-100-10-254.ap-southeast-1.compute.internal>
2025-12-14 13:49:00 +0000

    initial for jenkins share libs

--- jenkins-sharelibs/src/org/yudnk/builders/MavenBuilder.groovy
+++ jenkins-sharelibs/src/org/yudnk/builders/MavenBuilder.groovy
@@ -7,7 +7,7 @@ class MavenBuilder implements Builder, Serializable {
         script.echo "Build with maven...."
 
         def goals = config.goals ?:'clean package'
-        def profiles = config.profiles ? "-P${config.profiles.join(',)}" : ''
+        def profiles = config.profiles ? "-P${config.profiles.join(',')}" : ''
         def additionalArgs = config.additionalArgs?: ''
 
         script.sh """
