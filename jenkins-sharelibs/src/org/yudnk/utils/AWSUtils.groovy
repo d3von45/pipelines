f6e0ada7079fc45f7fa86a8bb6951042618aa6a6
EC2 Default User <ec2-user@ip-10-100-10-254.ap-southeast-1.compute.internal>
2026-01-02 08:32:03 +0000

    create class AWSUtils

---
+++ jenkins-sharelibs/src/org/yudnk/utils/AWSUtils.groovy
@@ -0,0 +1,16 @@
+package org.yudnk.utils
+
+class AWSUtils implements Serializable{
+
+    String getSSMParameter(script, String ssmPath){
+        
+        def rs = script.sh(
+            script: "aws ssm get-parameters --name \"${$ssmPath}\" --with-decryption --query \"Parameters[].Value\" --output text",
+            returnStdout: true
+        ).trim()
+
+        return rs
+
+    }
+
+}
